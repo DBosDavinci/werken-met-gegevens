494e60ef9d11f1502a9ac19f1644e6b5e7d9ec82
DBosDavinci <99069008@mydavinci.nl>
2021-09-06 12:19:56 +0200

    python calculations

---
+++ Feestlunch.py
@@ -0,0 +1,8 @@
+croissantAmount = 17
+croissantPrice = 0.39
+stokbrodenAmount = 2
+stokbrodenPrice = 2.78
+kortingsbonnenAmount = 3
+kortingbonnenPrice = 0.50
+
+print((croissantAmount*croissantPrice)+(stokbrodenAmount*stokbrodenPrice)-(kortingsbonnenAmount*kortingbonnenPrice))
---
+++ de Speelhal-dag.py
@@ -0,0 +1,7 @@
+ppl = 3
+ppd = 7.45
+vrtime = 45
+vrtimeper = 5
+vrpriceper5 = 0.37
+
+print((ppl*ppd)+((vrtime/vrtimeper)*vrpriceper5*ppl))
