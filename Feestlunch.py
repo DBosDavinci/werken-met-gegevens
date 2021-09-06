4049c5a23f875e4f233abffbea61eb10e727da9d
DBosDavinci <99069008@mydavinci.nl>
2021-09-06 15:33:44 +0200

    'Beschrijving'

--- Feestlunch.py
+++ Feestlunch.py
@@ -5,4 +5,6 @@ stokbrodenPrice = 2.78
 kortingsbonnenAmount = 3
 kortingbonnenPrice = 0.50
 
-print((croissantAmount*croissantPrice)+(stokbrodenAmount*stokbrodenPrice)-(kortingsbonnenAmount*kortingbonnenPrice))
+totaal = ((croissantAmount*croissantPrice)+(stokbrodenAmount*stokbrodenPrice)-(kortingsbonnenAmount*kortingbonnenPrice))
+
+print('De feestlunch kost je bij de bakker', totaal, 'euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
--- de Speelhal-dag.py
+++ de Speelhal-dag.py
@@ -4,4 +4,6 @@ vrtime = 45
 vrtimeper = 5
 vrpriceper5 = 0.37
 
-print((ppl*ppd)+((vrtime/vrtimeper)*vrpriceper5*ppl))
+totalprice = ((ppl*ppd)+((vrtime/vrtimeper)*vrpriceper5*ppl))
+
+print('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar', totalprice,'euro')
